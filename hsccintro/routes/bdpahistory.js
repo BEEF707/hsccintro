9f578e835e80affdf06f8ece187c3f6d336ee723
BEEF707 <jadakenner64@gmail.com>
2025-02-22 18:53:48 +0000

    BEEF707

--- hsccintro/routes/bdpahistory.js
+++ hsccintro/routes/bdpahistory.js
@@ -5,11 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('bdpahistory', { title: 'bdpahistory' });
 });
-<li><a href="/"> <IMG SRC="IMAGES/BDPALOGO.PNG" HEIGHT="25"> > </A></
-<li><a href="/"> HOME </A></LI>
-<li><a href="/BDPAHISTORY"> BDPA HISTORY</a></li>
-<li><a href="#">SAMPLE FORM </a></li>
-<!-- END NAVBAR -->
-<WebGL2RenderingContext.,br.
-<br></br><br>
+
 module.exports = router;
--- hsccintro/routes/index.js
+++ hsccintro/routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if (prosses.env.CONSOLE_DEBUG){
+    console.log("debugging enabled");
+  }
   res.render('index', { title: 'Express' });
 });
 
