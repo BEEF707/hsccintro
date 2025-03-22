7ac31934a895cb6ec3363d1d315fe3f121ddd8dd
BEEF707 <jadakenner64@gmail.com>
2025-03-22 17:33:00 +0000

    update

--- hsccintro/middleware/APIRequests.js
+++ hsccintro/middleware/APIRequests.js
@@ -1,3 +1,4 @@
+
 const https = require('https');
 
 // Starting module... It should have at least five functions when we are done
@@ -31,5 +32,35 @@ module.exports = {
  
       req.end();
     }); //end the Promise
-  } //End the getWithBearerToken function
+  }, //End the getWithBearerToken function
+ 
+  postWithBearerToken: function(url,token,data){
+
+    const options = {
+        method:"POST",
+        headers: {
+            Authorization: `Bearer ${token}`  //These are forward ticks, not quotes!
+            // Will we need content type?
+        }
+    }; //End Setting our headers...
+
+    return new Promise((resolve,reject) => {
+        const req=http.request(url,options,res=>{
+            let data = '';
+            res.on('data', chunk => {
+              data += chunk;
+            });
+            res.on('end', () => {
+              resolve(JSON.parse(data));
+            });
+          });
+     
+          req.on('error', error => {
+            reject(error);
+          });
+     
+          req.write(JSON.stringify(data));
+          req.end();
+        })//End Promise
+    }//End postWithBearerToken function
 }; //End module.exports
